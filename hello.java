fa7801fd5996851c52286e5f6541cb3f47f3461f
Pavan060502 <02pavankumarsrp@gmail.com>
2025-04-02 12:46:00 +0530

    first commit

--- hello.java
+++ hello.java
@@ -1,8 +1,7 @@
 class hello{
 public static void main(String []agrs){
 System.out.println("hello worldss...");
-System.out.println("hello worldss...");
-System.out.println("hello worldss...");
+
 System.out.println("hello worldss...");
 }
 }
